e7bc7c9703c38c9a71e733c6015f5e2470774875
Guilhem <bouryguilhem@gmail.com>
2023-12-22 15:32:31 +0100

    Log-in interface

--- src/main/java/com/isep/projectjavawallet/HelloApplication.java
+++ src/main/java/com/isep/projectjavawallet/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Log-in.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600 , 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/isep/projectjavawallet/HelloController.java
+++ src/main/java/com/isep/projectjavawallet/HelloController.java
@@ -1,14 +1,27 @@
 package com.isep.projectjavawallet;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Button No_Account;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button No_PW;
+    @FXML
+    public void handleMouseEnterBouton(MouseEvent event) {
+        Object source = event.getSource();
+        if (source instanceof Button) {
+            ((Button) source).setStyle(((Button) source).getStyle() + "-fx-underline: true;");
+        }
+    }
+    @FXML
+    public void handleMouseExitBouton(MouseEvent event) {
+        Object source = event.getSource();
+        if (source instanceof Button) {
+            ((Button) source).setStyle(((Button) source).getStyle() + "-fx-underline: false;");
+        }
     }
 }
